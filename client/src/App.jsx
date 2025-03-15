8e1a4e4a0104f4dcf4d42f02b77bafcf14dcbecd
Cosmin Vlasie <cvlasie@gmail.com>
2025-03-15 20:58:01 +0000

    v04_4_1 Removed unused pages and updated App.jsx
    - Removed pages used in the first prototype: CreateClassroom, CreateLesson, CreateReport, ViewLessons, ViewReports.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,39 +7,28 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import TeacherDashboard from './pages/TeacherDashboard'
-import CreateLesson from './pages/CreateLesson'
-import CreateReport from './pages/CreateReport'
-import ViewLessons from './pages/ViewLessons'
-import ViewReports from './pages/ViewReports'
 import ProtectedRoute from './components/ProtectedRoute'
-import CreateClassroom from './pages/CreateClassroom'
 import ClassroomDetail from './pages/ClassroomDetail'
 
-
-
 function App() {
-  
   return (
-      <Router>
-        <Routes>
-          {/* public route */}
-          <Route path="/" element={<Home />}/>
-          <Route path="/login" element={<Login />}/>
-          <Route path="/signup" element={<SignUp />}/>
-          {/* protected route */}
-          <Route element={<ProtectedRoute/>}>
-            <Route path="/dashboard" element={<TeacherDashboard />}/>
-          </Route>
-          <Route path="/create-lesson" element={<CreateLesson />} />
-          <Route path="/create-report" element={<CreateReport />} />
-          <Route path="/lessons" element={<ViewLessons />} />
-          <Route path="/reports" element={<ViewReports />} />
-          <Route path="/classroom/new" element={<CreateClassroom />} />
+    <Router>
+      <Routes>
+        {/* public routes */}
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/signup" element={<SignUp />}/>
+        
+        {/* protected routes */}
+        <Route element={<ProtectedRoute/>}>
+          <Route path="/dashboard" element={<TeacherDashboard />}/>
           <Route path="/classroom/:id" element={<ClassroomDetail />} />
-          {/* fallback route */}
-          <Route path="*" element={<Navigate to="/login" />}/>
-        </Routes>  
-      </Router>
+        </Route>
+
+        {/* fallback route */}
+        <Route path="*" element={<Navigate to="/login" />}/>
+      </Routes>  
+    </Router>
   )
 }
 
